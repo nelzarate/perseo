bd4b1390cb7c001f4d667753dc65a5483abff33e
nelzarate <nelsonzarate3185@gmail.com>
2020-09-14 20:15:19 -0400

    primer commit

---
+++ core/erp/tests.py
@@ -0,0 +1,21 @@
+from app.wsgi import *
+from core.erp.models import Type
+
+""" query = Type.objects.all()
+print (query)
+ """
+
+
+""" t = Type()
+t.name='senador'
+t.save() """
+
+""" t = Type.objects.get(id='4')
+t.name='diputado'
+t.save() 
+print(t.name) """
+#obj = Type.objects.filter(name__icontains='Dipu')
+for i in Type.objects.filter(name__contains='a'):
+    print("es asi asi" + i.name)
+    print("o  k")
+
